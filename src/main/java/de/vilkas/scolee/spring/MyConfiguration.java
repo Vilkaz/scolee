532069f785bb70a03fcf80e368fa64dfbff12b84
Vilkaz <admin@vilkas.de>
2018-09-06 10:02:48 +0200

    database = update no more create

--- src/main/java/de/vilkas/scolee/spring/MyConfiguration.java
+++ src/main/java/de/vilkas/scolee/spring/MyConfiguration.java
@@ -7,6 +7,7 @@ import de.vilkas.scolee.module.calendar.MeetingDataProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
